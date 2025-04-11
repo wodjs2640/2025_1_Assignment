30cea3d11a080f0b04d3ada33be8d99b41f6a4fb
wodjs2640 <47349711+wodjs2640@users.noreply.github.com>
2025-04-11 19:45:58 +0900

    3 초기세팅

--- HW4/Exercise3.ml
+++ HW4/Exercise3.ml
@@ -1,1 +1,5 @@
-let () = print_int 1
+type treasure = StarBox | NameBox of string
+type key = Bar | Node of key * key
+type map = End of treasure | Branch of map * map | Guide of string * map
+
+let getReady (m : map) : key list = []

b08d6d62661b10f46f558218f6bd4997d98992cf
wodjs2640 <47349711+wodjs2640@users.noreply.github.com>
2025-04-11 23:21:39 +0900

    임시 수정

--- HW4/Exercise4.ml
+++ HW4/Exercise4.ml
@@ -22,11 +22,7 @@ let collect_all_items (reqs : require list) : gift list =
        reqs)
 
 let rec mark_gifts (gifts : gift list) (arr : bool ref array) =
-  match gifts with
-  | g :: rest ->
-      arr.(g) := true;
-      mark_gifts rest arr
-  | [] -> ()
+  List.iter (fun g -> arr.(g) := true) gifts
 
 let rec process_cond (conds : cond list) (arr : bool ref array) =
   List.iter
@@ -43,29 +39,35 @@ let get_marked_gifts arr =
   |> List.filter_map (fun x -> x)
 
 let shoppingList (reqs : require list) : (id * gift list) list =
-  let a_conds, b_conds, c_conds, d_conds, e_conds =
-    match reqs with
-    | [ (_, a); (_, b); (_, c); (_, d); (_, e) ] -> (a, b, c, d, e)
-    | _ -> failwith "Invalid Input"
-  in
+  let all_ids = [ A; B; C; D; E ] in
   let all_gifts = collect_all_items reqs in
   let gift_count = List.length all_gifts in
-  let a_arr = Array.make gift_count (ref false) in
-  let b_arr = Array.make gift_count (ref false) in
-  let c_arr = Array.make gift_count (ref false) in
-  let d_arr = Array.make gift_count (ref false) in
-  let e_arr = Array.make gift_count (ref false) in
+  let process_id id =
+    let conds =
+      match List.find_opt (fun (req_id, _) -> req_id = id) reqs with
+      | Some (_, conds) -> conds
+      | None -> failwith "Invalid Input"
+    in
+    let arr = Array.make gift_count (ref false) in
+    process_cond conds arr;
+    (id, get_marked_gifts arr)
+  in
+  List.map process_id all_ids
 
-  process_cond a_conds a_arr;
-  process_cond b_conds b_arr;
-  process_cond c_conds c_arr;
-  process_cond d_conds d_arr;
-  process_cond e_conds e_arr;
-
-  let a_gifts = get_marked_gifts a_arr in
-  let b_gifts = get_marked_gifts b_arr in
-  let c_gifts = get_marked_gifts c_arr in
-  let d_gifts = get_marked_gifts d_arr in
-  let e_gifts = get_marked_gifts e_arr in
-
-  [ (A, a_gifts); (B, b_gifts); (C, c_gifts); (D, d_gifts); (E, e_gifts) ]
+let () =
+  let rl =
+    [
+      (A, []);
+      (B, [ Items [ 1; 2 ] ]);
+      (C, [ Items [ 3 ] ]);
+      (D, [ Items [ 2; 3 ] ]);
+      (E, []);
+    ]
+  in
+  let ans = shoppingList rl in
+  let print_gift_list id gifts =
+    Printf.printf "%s, [" (match id with A -> "A" | B -> "B" | C -> "C" | D -> "D" | E -> "E");
+    List.iter (fun g -> Printf.printf "%d " g) gifts;
+    print_endline "]"
+  in
+  List.iter (fun (id, gifts) -> print_gift_list id gifts) ans

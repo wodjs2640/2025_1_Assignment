4caf5e00f066981796f996cc7c985cf5f8e42fe6
wodjs2640 <47349711+wodjs2640@users.noreply.github.com>
2025-04-11 19:26:26 +0900

    format 추가

--- HW4/Exercise4.ml
+++ HW4/Exercise4.ml
@@ -1,1 +1,12 @@
-let () = print_int 1
+type require = id * cond list
+
+and cond =
+  | Items of gift list (* gifts *)
+  | Same of id (* same gifts as for id *)
+  | Common of cond * cond (* common gifts *)
+  | Except of cond * cond (* exclude gifts *)
+
+and gift = int (* gift id *)
+and id = A | B | C | D | E (* pig names *)
+
+let shoppingList (rl : require list) : (id * gift list) list = []
